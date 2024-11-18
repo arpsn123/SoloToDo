46bb9007245c063795db50e4316f6dc9bb8cfde0
Arpan Nath <arpan.nath1001@gmail.com>
2024-11-18 10:15:54 +0530

    added first manual blade webpage

---
+++ resources/views/first.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the first blade webpage i am building;
+</body>
+</html>
